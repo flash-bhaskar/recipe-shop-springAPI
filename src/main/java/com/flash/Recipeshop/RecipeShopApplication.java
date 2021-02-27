f26fd2f69a991029319dd87292e615ed6d0cad02
Flash_bhaskar <bhaskaras1999@gmail.com>
2021-02-26 21:37:55 -0800

    Recipe shop Initialisation

---
+++ src/main/java/com/flash/Recipeshop/RecipeShopApplication.java
@@ -0,0 +1,13 @@
+package com.flash.Recipeshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flash/Recipeshop/RecipeShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flash.Recipeshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
